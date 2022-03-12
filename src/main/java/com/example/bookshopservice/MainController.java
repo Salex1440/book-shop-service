70ad51541509b03c430717ec40ad5004182cb345
Salex1440 <ShutovAlexandr@yandex.ru>
2022-03-12 17:59:20 +0300

    Created delete methods for Author and Publisher. Created PUT methods for a book.

--- src/main/java/com/example/bookshopservice/MainController.java
+++ src/main/java/com/example/bookshopservice/MainController.java
@@ -103,9 +103,66 @@ public class MainController {
     {
         Book book = bookRepository.findBookByName(name);
         if (book == null) {
-            return String.format("Book %s doesn't exist", name);
+            return String.format("Book %s doesn't exist!", name);
         }
         bookRepository.delete(book);
-        return String.format("Book \"%s\" was deleted.", book.getName());
+        return String.format("Book \"%s\" was deleted.", name);
+    }
+
+    @DeleteMapping(path = "/delete_author")
+    public @ResponseBody String deleteAuthor(@RequestParam String name)
+    {
+        Author author = authorRepository.findAuthorByName(name);
+        if (author == null) {
+            return String.format("Author %s doesn't exist!", name);
+        }
+        authorRepository.delete(author);
+        return String.format("Author \"%s\" was deleted.", name);
+    }
+
+    @DeleteMapping(path = "/delete_publisher")
+    public @ResponseBody String deletePublisher(@RequestParam String name) {
+        Publisher publisher= publisherRepository.findPublisherByName(name);
+        if (publisher == null) {
+            return String.format("Publisher %s doesn't exist!", name);
+        }
+        publisherRepository.delete(publisher);
+        return String.format("Publisher \"%s\" was deleted.", name);
+    }
+
+    @PutMapping(path= "/change_author")
+    public @ResponseBody String changeAuthor(@RequestParam String bookName,
+                                             @RequestParam String authorName) {
+        Book book = bookRepository.findBookByName(bookName);
+        if (book == null) {
+            return String.format("There is no book with this name: %s.", bookName);
+        }
+        Author author = authorRepository.findAuthorByName(authorName);
+        if (author == null) {
+            return String.format("There is no author with this name: %s", authorName);
+        }
+        Author prevAuthor = book.getAuthorId();
+        book.setAuthorId(author);
+        bookRepository.save(book);
+        return String.format("Author of book \"%s\" was changed from %s to %s.",
+                bookName, prevAuthor.getName(), author.getName());
+    }
+
+    @PutMapping(path= "/change_publisher")
+    public @ResponseBody String changePublisher(@RequestParam String bookName,
+                                             @RequestParam String publisherName) {
+        Book book = bookRepository.findBookByName(bookName);
+        if (book == null) {
+            return String.format("There is no book with this name: %s.", bookName);
+        }
+        Publisher publisher = publisherRepository.findPublisherByName(publisherName);
+        if (publisher == null) {
+            return String.format("There is no publisher with this name: %s", publisherName);
+        }
+        Publisher prevPublisher = book.getPublisherId();
+        book.setPublisherId(publisher);
+        bookRepository.save(book);
+        return String.format("Publisher of book \"%s\" was changed from %s to %s.",
+                bookName, prevPublisher.getName(), publisher.getName());
     }
 }
