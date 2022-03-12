c15d5bf8fd8750d6a0a44fbfbee955568694cef2
Salex1440 <ShutovAlexandr@yandex.ru>
2022-03-12 14:29:44 +0300

    Added deleteBook method. Increased server fault tolerance.

--- src/main/java/com/example/bookshopservice/BookRepository.java
+++ src/main/java/com/example/bookshopservice/BookRepository.java
@@ -11,9 +11,10 @@ public interface BookRepository extends CrudRepository <Book, Integer> {
     @Query("SELECT b FROM Book b WHERE b.name= :name")
     Book findBookByName(@Param("name") String name);
 
-    @Query("SELECT b FROM Book b WHERE b.author_id = :author_id")
+    /*@Query("SELECT b FROM Book b WHERE b.author_id = :author_id")
     List<Book> findBookByAuthor(@Param("author_id") Integer author_id);
 
     @Query("SELECT b FROM Book b WHERE b.publisher_id = :publisher_id")
     List<Book> findBookByPublisher(@Param("publisher_id") Integer publisher_id);
+    */
 }
--- src/main/java/com/example/bookshopservice/MainController.java
+++ src/main/java/com/example/bookshopservice/MainController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @Controller
-@RequestMapping(path = "/books")
+@RequestMapping(path = "/bookshop")
 public class MainController {
     @Autowired
     private BookRepository bookRepository;
@@ -23,15 +23,29 @@ public class MainController {
                                         @RequestParam String authorName,
                                         @RequestParam String publisherName)
     {
-        Book nb = new Book();
+        Book nb = bookRepository.findBookByName(bookName);
+        if (nb != null) {
+            return String.format("Book \"%s\" exists!", bookName);
+        }
+        nb = new Book();
         nb.setName(bookName);
         Author author = authorRepository.findAuthorByName(authorName);
         nb.setAuthorId(author);
         Publisher publisher = publisherRepository.findPublisherByName(publisherName);
         nb.setPublisherId(publisher);
         String template = "Book: %s; Author: %s; Publisher: %s;";
-        String aName = author.getName();
-        String pName = publisher.getName();
+        String aName, pName;
+        if (author == null) {
+            aName = "null";
+        } else {
+            aName = author.getName();
+        }
+        if (publisher == null) {
+            pName = "null";
+        } else {
+            pName = publisher.getName();
+        }
+        bookRepository.save(nb);
         String message = String.format(template, bookName, aName, pName);
         return message;
     }
@@ -43,6 +57,7 @@ public class MainController {
         // @RequestParam means it is a parameter from the GET or POST request.
         Author na = new Author();
         na.setName(authorName);
+        authorRepository.save(na);
         return "Author added.";
     }
 
@@ -53,25 +68,36 @@ public class MainController {
         // @RequestParam means it is a parameter from the GET or POST request.
         Publisher np = new Publisher();
         np.setName(publisherName);
+        publisherRepository.save(np);
         return "Author added.";
     }
 
-    @GetMapping(path = "/getAllBooks")
+    @GetMapping(path = "/get_all_books")
     public @ResponseBody Iterable<Book> getAllBooks()
     {
         return bookRepository.findAll();
     }
 
-    @GetMapping(path = "/getAllAuthors")
+    @GetMapping(path = "/get_all_authors")
     public @ResponseBody Iterable<Author> getAllAuthors()
     {
         return authorRepository.findAll();
     }
 
-    @GetMapping(path = "/getAllPublishers")
+    @GetMapping(path = "/get_all_publishers")
     public @ResponseBody Iterable<Publisher> getAllPublishers()
     {
         return publisherRepository.findAll();
     }
 
+    @DeleteMapping(path = "/delete_book")
+    public @ResponseBody String deleteBook(@RequestParam String name)
+    {
+        Book book = bookRepository.findBookByName(name);
+        if (book == null) {
+            return String.format("Book %s doesn't exist", name);
+        }
+        bookRepository.delete(book);
+        return String.format("Book \"%s\" was deleted.", book.getName());
+    }
 }
